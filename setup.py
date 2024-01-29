4be4a20f2cf6cc2350f68d2862739e960be167d9
defreitasL <lucas12.fp@hotmail.com>
2024-01-29 09:25:29 +0100

    Building pip

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='SMCResultPlotHub',
     version='0.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(),
+    package_dir={'': 'src/SMCResultPlotHub'},
     include_package_data=True,
     install_requires=[
         'numpy',
