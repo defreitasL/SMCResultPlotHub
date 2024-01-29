da03bc2ddbcf39502f28ac7ba436160dd3e5f876
defreitasL <lucas12.fp@hotmail.com>
2024-01-29 09:19:44 +0100

    Building pip

--- build/lib/src/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# src/utils/__init__.py
-
-# Import utility functions from your package here
-from .utils import wMOORE, Hunt, memclear
--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='SMCResultPlotHub',
     version='0.1',
-    packages=find_packages(where='SMCResultPlotHub'),
-    package_dir={'': 'SMCResultPlotHub'},
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     include_package_data=True,
     install_requires=[
         'numpy',
